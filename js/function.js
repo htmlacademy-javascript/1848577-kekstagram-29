db597888f5bfb59093c9e844fe51bdb8aa1de6a3
Никита <belokovilskij@yandex.ru>
2023-06-25 13:39:47 +0700

    Функции возвращаются

---
+++ js/function.js
@@ -0,0 +1,50 @@
+const isStringOverMaxLength = (string, maxLength) => string.length <= maxLength;
+
+isStringOverMaxLength('проверяемая строка', 20);
+
+const isPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toUpperCase();
+  let reversedString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    reversedString += normalizedString[i];
+  }
+  return normalizedString === reversedString;
+};
+
+isPalindrome('Кекс');
+
+const extractNumbers = (string) => {
+  string = string.toString();
+  let result = '';
+  let number;
+  for (let i = 0; i < string.length; i++) {
+    number = parseInt(string[i], 10);
+    if (!Number.isNaN(number)) {
+      result += number;
+    }
+  }
+
+  return Number(result) || NaN;
+};
+
+extractNumbers('2023 год');
+
+const isEnoighTime = (workStart, workEnd, meetingStart, meetingDuration) => {
+  const workStartSplited = workStart.split(':');
+  const workEndSplited = workEnd.split(':');
+  const meetingStartSplited = meetingStart.split(':');
+
+  const workStartMinutes = parseInt(workStartSplited[0], 10) * 60 + parseInt(workStartSplited[1], 10);
+  const workEndMinutes = parseInt(workEndSplited[0], 10) * 60 + parseInt(workEndSplited[1], 10);
+  const meetingStartMinutes = parseInt(meetingStartSplited[0], 10) * 60 + parseInt(meetingStartSplited[1], 10);
+  const meetingEnd = meetingStartMinutes + meetingDuration;
+
+  if (meetingStartMinutes >= workStartMinutes && meetingEnd <= workEndMinutes) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+isEnoighTime('08:00', '17:00', '14:00', 90);
--- js/util.js
+++ js/util.js
@@ -1,34 +1,3 @@
-const isStringOverMaxLength = (string, maxLength) => string.length <= maxLength;
-
-isStringOverMaxLength('проверяемая строка', 20);
-
-const isPalindrome = (string) => {
-  const normalizedString = string.replaceAll(' ', '').toUpperCase();
-  let reversedString = '';
-  for (let i = normalizedString.length - 1; i >= 0; i--) {
-    reversedString += normalizedString[i];
-  }
-  return normalizedString === reversedString;
-};
-
-isPalindrome('Кекс');
-
-const extractNumbers = (string) => {
-  string = string.toString();
-  let result = '';
-  let number;
-  for (let i = 0; i < string.length; i++) {
-    number = parseInt(string[i], 10);
-    if (!Number.isNaN(number)) {
-      result += number;
-    }
-  }
-
-  return Number(result) || NaN;
-};
-
-extractNumbers('2023 год');
-
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
