8f2e3a35bce0a89ec00d97636e16e1c4922cc3fb
Никита <belokovilskij@yandex.ru>
2023-07-13 20:16:50 +0700

    Правда или действие

--- js/form.js
+++ js/form.js
@@ -1,39 +1,21 @@
 import {isEscapeKey} from './util.js';
 
+const MAX_HASHTAG_COUNT = 5;
+const MAX_HASHTAG_LENGTH = 20;
+const VALID_HASHTAG_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+
 const imgUploadInput = document.querySelector('.img-upload__input');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
 const body = document.querySelector('body');
 const imgUploadForm = document.querySelector('.img-upload__form');
-const successTemplate = document.querySelector('#success').content;
-const errorTemplate = document.querySelector('#error').content;
+const hashtagsField = document.querySelector('.text__hashtags');
+const commentField = document.querySelector('.text__description');
 
-const successMessage = successTemplate.cloneNode(true);
-const errorMessage = errorTemplate.cloneNode(true);
-
-const onSuccessMessageClose = () => {
-  successMessage.remove();
-};
-
-const renderSuccessMessage = () => {
-  const successButton = successMessage.querySelector('.success__button');
-
-  body.append(successMessage);
-  successButton.addEventListener('click', onSuccessMessageClose);
-  document.addEventListener('keydown', onDocumentKeydownSuccessMessage);
-};
-
-const onErrorMessageClose = () => {
-  errorMessage.remove();
-};
-
-const renderErrorMessage = () => {
-  const errorButton = errorMessage.querySelector('.error__button');
-
-  body.append(errorMessage);
-  errorButton.addEventListener('click', onErrorMessageClose);
-  document.addEventListener('keydown', onDocumentKeydownSuccessMessage);
-};
+const pristine = new Pristine(imgUploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+});
 
 const OnUploadOverlayOpen = () => {
   imgUploadOverlay.classList.remove('hidden');
@@ -43,42 +25,42 @@ const OnUploadOverlayOpen = () => {
 };
 
 function onUploadCancel () {
+  imgUploadForm.reset();
+  pristine.reset();
   imgUploadOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
-  imgUploadInput.removeEventListener('change', OnUploadOverlayOpen);
+  document.removeEventListener('keydown', onDocumentKeydown);
 }
 
+const isTextFieldFocused = () => document.activeElement === hashtagsField || document.activeElement === commentField;
+
 function onDocumentKeydown (evt) {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isTextFieldFocused()) {
     evt.preventDefault();
     onUploadCancel();
   }
 }
 
-function onDocumentKeydownSuccessMessage (evt) {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    onSuccessMessageClose();
-  }
-}
+const normalizeHashags = (tagString) => tagString.trim().split(' ').filter((tag) => Boolean(tag.length));
+
+const hasValidLength = (value) => normalizeHashags(value).every((hashtag) => hashtag.length <= MAX_HASHTAG_LENGTH);
+
+const hasValidCount = (value) => normalizeHashags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasValidSymbols = (value) => normalizeHashags(value).every((hashtag) => VALID_HASHTAG_SYMBOLS.test(hashtag));
+
+const hasUniqueTags = (value) => {
+  const lowerCaseHashtags = normalizeHashags(value).map ((hashtag) => hashtag.toLowerCase());
+  return lowerCaseHashtags.length === new Set(lowerCaseHashtags).size;
+};
+
+pristine.addValidator(hashtagsField, hasValidLength, `Максимальная длина одного хэш-тега ${MAX_HASHTAG_LENGTH} символов, включая решётку`, 4, true);
+pristine.addValidator(hashtagsField, hasValidCount, `Нельзя указать больше ${MAX_HASHTAG_COUNT} хэш-тегов`, 3, true);
+pristine.addValidator(hashtagsField, hasValidSymbols, 'Недопустимый хэш-тег', 2, true);
+pristine.addValidator(hashtagsField, hasUniqueTags, 'Один и тот же хэш-тег не может быть использован дважды', 1, true);
 
 imgUploadInput.addEventListener('change', OnUploadOverlayOpen);
-
-const pristine = new Pristine(imgUploadForm);
-
-const validateComment = (value) => value.length <= 140;
-
-
-pristine.addValidator(imgUploadForm.querySelector('.text__hashtags'));
-pristine.addValidator(imgUploadForm.querySelector('.text__description'), validateComment, 'длина менее 140 символов');
-
 imgUploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    renderSuccessMessage();
-  } else {
-    renderErrorMessage();
-  }
+  pristine.validate();
 });
